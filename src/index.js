9b3c56dc68733452ff0d24c08e9d27b12f0f5f88
Chitre <schitre@paypal.com>
2018-10-19 23:16:58 -0700

    convert functional to class based component for app and add youtube search api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'XXXX'; //Youtube search api key 
+const API_KEY = 'XXXXX'; //Youtube search api key 
 
 //Create a new Component, this component should produce some html
-const App =  () => { //used new ES6 fat arrow function syntax
-    return (
-    <div>
-        <SearchBar/>
-    </div>  //<div>Hi!</div> this is JSX; webpack and babel do some transpiling of the code to convert it to html
-    );
+class App extends Component { //used new ES6 fat arrow function syntax
+    constructor(props){
+        super(props);
+        this.state = {
+            videos: []
+        };
+        YTSearch({ key: API_KEY, term: 'surfboards'},  (videos) => {
+            this.setState({videos});  //same as this.setState({videos: videos});
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>  //<div>Hi!</div> this is JSX; webpack and babel do some transpiling of the code to convert it to html
+            );
+    } 
     //it will eventually look different as html
     //JSX cannot be directly interpreted by the browser, we need webpack and babel to convert to vanilla JS and HTML
 }
