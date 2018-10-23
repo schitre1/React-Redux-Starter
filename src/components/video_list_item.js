5ccc87e0e1665759153f1acfe3038049885c9140
Chitre <schitre@paypal.com>
2018-10-22 19:53:33 -0700

    Adding video detail and switching videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => { //or have props here in top and comment below.
+const VideoListItem = ({video, onVideoSelect}) => { //or have props here in top and comment below.
     //const video = props.video; Use es6 equivalent
     const imageUrl = video.snippet.thumbnails.default.url;
     return (
-        <li className="list-group-item">
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
             <div className="video-list media">
                 <div className="media-left">
                     <img className = "media-object" src={imageUrl}></img>
--- src/index.js
+++ src/index.js
@@ -4,25 +4,33 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'XXX'; //Youtube search api key 
+const API_KEY = 'AIzaSyABjojdD7ElP80oKjtyyVA_wPpQ7n8RvrQ'; //Youtube search api key 
 
 //Create a new Component, this component should produce some html
 class App extends Component { //used new ES6 fat arrow function syntax
     constructor(props){
         super(props);
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null
         };
         YTSearch({ key: API_KEY, term: 'surfboards'},  (videos) => {
-            this.setState({videos});  //same as this.setState({videos: videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });  //same as this.setState({videos: videos});
         });
     }
     render(){
         return (
             <div>
                 <SearchBar/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video= {this.state.selectedVideo}/>
+                <VideoList 
+                onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
             </div>  //<div>Hi!</div> this is JSX; webpack and babel do some transpiling of the code to convert it to html
             );
     } 
